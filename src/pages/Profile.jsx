6d28d5edcaa0714b47f30f5aad71bbc09bf798b4
sukhendup <sukhendupatra007@gmail.com>
2023-08-14 18:00:53 +0530

    fouth commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,8 +3,8 @@ import { Context} from "../main";
 import Loader from '../components/Loader';
 import { Navigate } from 'react-router-dom';
 const Profile = () => {
-  const {loading,user} = useContext(Context)
-  return loading ? (<Loader />) :(
+  const {user} = useContext(Context)
+  return (
       <div className='pro'>
           <h1>Username: {user?.name}</h1>
           <h3>E-mail : {user?.email}</h3>
