8464386cf1e1dc9d061f0551a88efc20fe257f32
sukhendup <sukhendupatra007@gmail.com>
2023-08-14 17:06:35 +0530

    second commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,13 +3,13 @@ import { Context} from "../main";
 import Loader from '../components/Loader';
 import { Navigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
-const Profile = () => {
-  const {isAuth,loading,user,count} = useContext(Context)
-  // if (!isAuth) return <Navigate to={"/login"} />;
+import axios from 'axios';
+const Profile = async () => {
+  const {isAuth,loading,user} = useContext(Context)
   return loading ? (<Loader />) :(
       <div className='pro'>
-          <h1>Username : {user?.name}</h1>
-          <h3>E-mail : {user?.email}</h3>
+          <h1>{user?.name}</h1>
+          <h3>{user?.email}</h3>
       </div>
   );
 };
